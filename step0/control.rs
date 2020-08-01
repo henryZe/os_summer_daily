9a8e471a8e68c2e9de69dab305e0d5d529914f79
Zeng Heng <henryZ_e@163.com>
2020-08-01 14:18:59 +0800

    day 10

--- step0/control.rs
+++ step0/control.rs
@@ -227,5 +227,70 @@ fn main() {
     }
 
     // 8.5.1.4. structs
+    struct Foo {
+        x: (u32, u32),
+        y: u32,
+    }
+
+    // Try changing the values in the struct to see what happens
+    let foo = Foo { x: (3, 2), y: 3 };
+
+    match foo {
+        Foo { x: (1, b), y } => println!("First of x is 1, b = {},  y = {} ", b, y),
+
+        // you can destructure structs and rename the variables,
+        // the order is not important
+        Foo { y: 2, x: i } => println!("y is 2, i = {:?}", i),
+
+        // and you can also ignore some variables:
+        Foo { y, .. } => println!("y = {}, we don't care about x", y),
+
+        // this will give an error: pattern does not mention field `x`
+        // Foo { y } => println!("y = {}", y),
+    }
+
+    // 8.5.2. Guards
+    let pair = (2, -2);
+
+    println!("Tell me about {:?}", pair);
+    match pair {
+        (x, y) if x == y => println!("These are twins"),
+        // The ^ `if condition` part is a guard
+        (x, y) if x + y == 0 => println!("Antimatter, kaboom!"),
+        (x, _) if x % 2 == 1 => println!("The first one is odd"),
+        _ => println!("No correlation..."),
+    }
+
+    // 8.5.3. Binding
+    fn age() -> u32 {
+        15
+    }
+    println!("Tell me what type of person you are");
+    match age() {
+        0             => println!("I haven't celebrated my first birthday yet"),
+        // Could `match` 1 ..= 12 directly but then what age
+        // would the child be? Instead, bind to `n` for the
+        // sequence of 1 ..= 12. Now the age can be reported.
+        n @ 1  ..= 12 => println!("I'm a child of age {:?}", n),
+        n @ 13 ..= 19 => println!("I'm a teen of age {:?}", n),
+        // Nothing bound. Return the result.
+        n             => println!("I'm an old person of age {:?}", n),
+    }
+
+    fn some_number() -> Option<u32> {
+        Some(43)
+    }
+    match some_number() {
+        // Got `Some` variant, match if its value, bound to `n`,
+        // is equal to 42.
+        Some(n @ 42) => println!("The Answer: {}!", n),
+        // Match any other number.
+        Some(n)      => println!("Not interesting... {}", n),
+        // Match anything else (`None` variant).
+        _            => println!("Not Some..."),
+    }
+
+    // 8.6. if let
     
+    // 8.7. while let
 }
